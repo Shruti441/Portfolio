cbc3faddad6ae92559dda392d81aa3d3722ad4bf
Shruti441 <varshneyshruti2003@gmail.com>
2025-04-24 15:43:59 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { LoadingScreen } from "./components/loadingscreen";
+import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
